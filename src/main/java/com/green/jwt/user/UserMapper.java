dd923691d4b093e08337e55b56acb0fa77ae15ac
SongEunA <dmsdktt@gmail.com>
2025-01-16 12:44:30 +0900

    로그인 작업

--- src/main/java/com/green/jwt/user/UserMapper.java
+++ src/main/java/com/green/jwt/user/UserMapper.java
@@ -1,10 +1,16 @@
 package com.green.jwt.user;
 
+import com.green.jwt.user.model.UserSelOne;
+import com.green.jwt.user.model.UserSignInReq;
 import com.green.jwt.user.model.UserSignUpReq;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
     int insUser(UserSignUpReq req);
     int insUserRole(UserSignUpReq req);
+    // Optional은 Null 체크를 위한 if문 없이 NPE(NullPointerException)이 발생하지 않는 간결하고 안전한 코드를 작성하는 것이 가능
+    Optional<UserSelOne> selUserWithRoles(UserSignInReq req);
 }
--- src/main/java/com/green/jwt/user/UserService.java
+++ src/main/java/com/green/jwt/user/UserService.java
@@ -1,12 +1,22 @@
 package com.green.jwt.user;
 
+import com.green.jwt.config.CookieUtils;
+import com.green.jwt.config.JwtConst;
+import com.green.jwt.config.jwt.JwtTokenProvider;
+import com.green.jwt.config.jwt.JwtUser;
+import com.green.jwt.user.model.UserSelOne;
+import com.green.jwt.user.model.UserSignInReq;
+import com.green.jwt.user.model.UserSignInRes;
 import com.green.jwt.user.model.UserSignUpReq;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
+
+import java.time.Duration;
 
 @Slf4j
 @Service
@@ -15,6 +25,9 @@ public class UserService {
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
     private final TransactionTemplate transactionTemplate; //DI
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CookieUtils cookieUtils;
+    private final JwtConst jwtConst;
 
     public void SignUp(UserSignUpReq req) {
         String hashedPw = passwordEncoder.encode(req.getPw());
@@ -28,5 +41,29 @@ public class UserService {
         });
     }
 
+    public UserSignInRes signIn(UserSignInReq req, HttpServletResponse response) {
+        UserSelOne userSelOne = userMapper.selUserWithRoles(req).orElseThrow(() -> {
+            throw new RuntimeException("아이디를 확인해 주세요.");
+        });
+
+        if (!passwordEncoder.matches(req.getPw(), userSelOne.getPw())) { //(암호화 되지 않은 비밀번호, 암호화된 비밀번호)
+            throw new RuntimeException("비밀번호를 확인해 주세요.");
+        }
+
+        //AT, RT
+        JwtUser jwtUser = new JwtUser(userSelOne.getId(), userSelOne.getRoles());
+
+        String accessToken = jwtTokenProvider.generateAccessToken(jwtUser);
+        String refreshToken = jwtTokenProvider.generateRefreshToken(jwtUser);
+
+        //RT를 쿠키에 담는다.
+        cookieUtils.setCookie(response, jwtConst.getRefreshTokenCookieName(), refreshToken, jwtConst.getRefreshTokenCookieExpiry());
+
+        return UserSignInRes.builder()
+                .accessToken(accessToken)
+                .id(userSelOne.getId())
+                .name(userSelOne.getName())
+                .build();
+    }
 
 }
